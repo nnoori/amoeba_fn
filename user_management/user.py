51f590978848c121d00e1da411035923bcf604d1
ubuntu <ubuntu@ubuntu.(none)>
2013-07-22 16:04:45 -0400

    clean up user.py

--- user_management/user.py
+++ user_management/user.py
@@ -31,11 +31,11 @@ vpass = form.regexp(r".{3,20}$", 'must be between 3 and 20 characters')
 vemail = form.regexp(r".*@.*", "must be a valid email address")
 
 register_form = form.Form(
-form.Textbox("username", description="Username"),
-form.Textbox("email", vemail, description="E-Mail"),
-form.Password("password", vpass, description="Password"),
-form.Password("password2", description="Repeat password"),
-form.Button("submit", type="submit", description="Register"),
+form.Textbox("username", description="Username", value=''),
+form.Textbox("email", vemail, description="E-Mail",value=''),
+form.Password("password", vpass, description="Password",value=''),
+form.Password("password2", description="Repeat password",value=''),
+form.Button("submit", type="submit", description="Register",value=''),
 validators = [
     form.Validator("Passwords did't match", lambda i: i.password == i.password2)]
 )
